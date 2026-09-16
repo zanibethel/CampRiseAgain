bb4306e318d88fedfc613fd463b2e09299fae3b5
zanibethel <zanibethel@gmail.com>
2026-09-15 19:44:29 -0500

    Use managed logo in site header

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
 import HomeManagedPanel from "@/components/HomeManagedPanel";
+import ManagedLogo from "@/components/ManagedLogo";
 
 export default function SiteHeader() {
-  return <><header><div className="nav"><Link className="brand" href="/"><Image src="/3426A745-DDB8-4CB7-8BBC-9DC68C16B20A.png" alt="Camp Rise Again" width={48} height={48} priority /><span>Camp Rise Again</span></Link><nav className="navlinks" aria-label="Primary navigation"><Link href="/#about">About</Link><Link className="resources-link" href="/resources">Resources</Link><Link href="/#contact">Contact</Link><Link className="btn btn-primary" href="/apply">Apply</Link><Link className="btn btn-secondary" href="/volunteer">Volunteer</Link></nav></div></header><HomeManagedPanel /></>;
+  return <><header><div className="nav"><Link className="brand" href="/"><ManagedLogo width={48} height={48} priority /><span>Camp Rise Again</span></Link><nav className="navlinks" aria-label="Primary navigation"><Link href="/#about">About</Link><Link className="resources-link" href="/resources">Resources</Link><Link href="/#contact">Contact</Link><Link className="btn btn-primary" href="/apply">Apply</Link><Link className="btn btn-secondary" href="/volunteer">Volunteer</Link></nav></div></header><HomeManagedPanel /></>;
 }
